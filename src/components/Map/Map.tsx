fbeec7f135ef15eeb8fa7be1125f0f6bda78f9fd
Romulo Rodrigues <biorrodrigues@gmail.com>
2022-01-29 22:19:09 -0300

    Added dynamic rendering to the Map Component setting it up to not render under SSR

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,7 +1,12 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Map = (/* { center, zoom, markers } */) => (
-  <MapContainer center={[0, 0]} zoom={13} scrollWheelZoom={false}>
+  <MapContainer
+    center={[0, 0]}
+    zoom={3}
+    scrollWheelZoom={true}
+    style={{ height: '100%', width: '100%' }}
+  >
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
-import Map from 'components/Map/Map'
+import dynamic from 'next/dynamic'
+
+const Map = dynamic(() => import('components/Map/Map'), {
+  ssr: false
+})
 
 export default function Home() {
   return <Map />
